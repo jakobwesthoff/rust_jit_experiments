0dca739cf65b1c083598bf0d2ed3c5fdeda48a19
Jakob Westhoff <jakob@westhoffswelt.de>
2024-05-01 13:19:18 +0200

    Better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use anyhow::{anyhow, Result};
+use anyhow::{Context, Result};
 
-fn memory_executable_code(code: &Vec<u8>) -> Result<extern "C" fn() -> u64> {
+fn memory_map_executable_code(code: &Vec<u8>) -> Result<extern "C" fn() -> u64> {
     let func: extern "C" fn() -> u64 = unsafe {
         // 1. mmap to map read/write anonymous memory of size code
         let ptr = libc::mmap(
@@ -12,18 +12,16 @@ fn memory_executable_code(code: &Vec<u8>) -> Result<extern "C" fn() -> u64> {
             0,
         );
         if ptr == libc::MAP_FAILED {
-            // @TODO: format system error as a string
-            // let last_error = std::io::Error::last_os_error().raw_os_error().unwrap();
-            return Err(anyhow!("Memory mapping region for executable code failed"));
+            return Err(std::io::Error::last_os_error())
+                .context("memory mapping region for executable code");
         }
         // 2. copy code to memory
         std::ptr::copy_nonoverlapping(code.as_ptr(), ptr as *mut u8, code.len());
         // 3. mprotect the memory to read/exec
         let result = libc::mprotect(ptr, code.len(), libc::PROT_EXEC | libc::PROT_READ);
         if result == -1 {
-            // @TODO: format system error as a string
-            // let last_error = std::io::Error::last_os_error().raw_os_error().unwrap();
-            return Err(anyhow!("Making mapped memory executable failed"));
+            return Err(std::io::Error::last_os_error())
+                .context("making memory mapped region executable");
         }
         // 4. reinterpret_cast memory pointer to function signature
         std::mem::transmute(ptr)
@@ -33,9 +31,15 @@ fn memory_executable_code(code: &Vec<u8>) -> Result<extern "C" fn() -> u64> {
 }
 
 fn main() -> Result<()> {
-    let code: Vec<u8> = vec![0xB8, 0x42, 0x00, 0x00, 0x00, 0xc3];
-    let func = memory_executable_code(&code)?;
-    // 5. call the function
+    #[rustfmt::skip]
+    let code: Vec<u8> = vec![
+        // mov qword rax, 0x42
+        0xB8, 0x42, 0x00, 0x00, 0x00,
+        // ret
+        0xc3,
+    ];
+
+    let func = memory_map_executable_code(&code)?;
     let result = func();
     println!("Dynamic code returned 0x{:x}", result);
     Ok(())
